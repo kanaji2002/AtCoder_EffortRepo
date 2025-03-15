e918c7c951de702c010cca3df0529b05d43574d1
kanaji2002 <gellunika6478@icloud.com>
2025-03-15 22:44:53 +0900

    追加

---
+++ ABC396/A.cpp
@@ -0,0 +1,22 @@
+using namespace std;
+#include <bits/stdc++.h>
+//https://atcoder.jp/contests/abc396/tasks/abc396_a
+#include <iostream>
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        int a[100];
+        cin >> a[i] ;
+        if (a[i]==a[i-1] && a[i]==a[i-2]){
+            cout << "Yes" << endl;
+            return 0;
+        }
+        if(i==n){
+            cout << "No" << endl;
+        }
+
+    }
+}
+
+
