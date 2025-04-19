7464c53f8068847ad1e33ee5d36c3041595197b5
kanaji <gellunika6478@icloud.com>
2025-04-20 02:00:11 +0900

    がんばる

---
+++ ABC401/A.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0;i<(int)(n);i++)
+using ll = long long;
+
+int main(){
+
+int N,R;
+cin >> N ;
+
+if(400%N==0)
+  cout << 400/N << endl;
+else
+cout << -1 << endl;
+
+
+return 0;
+}
