6fa04379b870d78fbdde7696ff1aefc049d3068f
kanaji <gellunika6478@icloud.com>
2025-04-29 15:30:36 +0900

    2分でできた．

---
+++ ABC176/A.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(long long i=0; i<n; i++)
+
+int main() {
+
+int M;
+cin >> M;
+cout << 24-M+24 << endl;
+    return 0;
+}
