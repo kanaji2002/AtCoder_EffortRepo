f2f159fb596e5b482c6ced97d76409c13b3c3498
kanaji <gellunika6478@icloud.com>
2025-05-05 23:20:17 +0900

    かしこ．

--- ABC391/A.cpp
+++ ABC391/A.cpp
@@ -1,24 +1,35 @@
 #include <iostream>
+#include <bits/stdc++.h>
 #include <vector>
+#include <map>
+#include <algorithm>
+#include <atcoder/dsu>
+using namespace atcoder;
 using namespace std;
 
-const int MOD = 1000000000;
+#define rep(i,n) for(int i=0; i<n; i++)
 
 int main() {
-    int n, k, X;
-    cin >> n >> k >> X;
-    int tem;
-    
-    for (int i = 1; i <= n; i++) {
-      cin >> tem;
-      cout << tem << endl;
-      if(i==k){
-      cout << X <<endl;
-      
-      }
-      
-    }
-
+string S;
+cin >> S;
+string O;
+if(S[0]=='N'){
+  O+='S';
+  if(S[1]=='E')O+='W';
+  else if(S[1]=='W')O+='E';
+}
+else if(S[0]=='S'){
+    O+='N';
+  if(S[1]=='E')O+='W';
+  else if(S[1]=='W')O+='E';
+}
+else if(S[0]=='W'){
+  O+='E';
+}
+else if(S[0]=='E'){
+O+='W';
+}
+cout << O << endl;;
 
     return 0;
 }
