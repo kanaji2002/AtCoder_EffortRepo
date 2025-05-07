6fc4205604a2ac6ca8241d3e08fb7664d2ee25c8
kanaji <gellunika6478@icloud.com>
2025-05-07 12:21:47 +0900

    おもしろい．タイル．放送を見て理解．

---
+++ ABC359/C.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+int main() {
+    long Sx, Sy, Tx, Ty;
+    cin >> Sx >> Sy >> Tx >> Ty;
+    // タイルの左側に寄せておく
+    if ((Sx + Sy) % 2 == 1) {
+        --Sx;
+    }
+    if ((Tx + Ty) % 2 == 1) {
+        --Tx;
+    }
+    // 答えは (|Sy - Ty| + max(|Sx - Tx|, |Sy - Ty|)) / 2
+    long Dx = abs(Sx - Tx);
+    long Dy = abs(Sy - Ty);
+    
+    cout << (Dy + max(Dx, Dy)) / 2 << endl;
+    return 0;
+}
