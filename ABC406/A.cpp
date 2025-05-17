64e6a94b517305434298b173bc503983d3672ee0
kanaji <gellunika6478@icloud.com>
2025-05-17 22:21:36 +0900

    ok

---
+++ ABC406/A.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i,n) for(int i=0; i<n; i++)
+
+
+int main(void){
+  
+int A,B,C,D;
+cin >> A >> B >> C >> D;
+if(A == C){
+  if(B > D)cout << "Yes" <<endl;
+  else cout << "No" <<endl;
+}
+
+else if(A > C)cout << "Yes" <<endl;
+else cout << "No" <<endl;
+
+
+  
+  
+
+    return 0;
+}
+
