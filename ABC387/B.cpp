a991539a8d14efff322768559b1d49096f49be8f
kanaji2002 <rinntarou2002520@gmail.com>
2025-10-22 10:30:38 +0700

    B

---
+++ ABC387/B.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <string>
+
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int i=0; i<n; ++i)
+int main() {
+
+string s;
+
+cin >> s ;
+
+rep(i,s.size()){
+    if(s[i]=='0')
+    cnt++;
+}
+
+return 0;
+}
